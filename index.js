f2b2d4825afde79962db5aadaa467b406dcdbac9
Franklyn Nussie <godfather.franklyn@gmail.com>
2023-03-31 15:18:45 +0100

    fix

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 require("dotenv").config({ path: __dirname + "/.env" })
+const fs = require("fs")
+const tls = require('tls')
+const https = require('https')
 const express = require("express")
 const cors = require("cors")
 const helmet = require('helmet')
 const httpProxy = require('http-proxy');
 const HttpProxyRules = require('http-proxy-rules');
 
-
-
-
 const app = express()
-const server = require("http").Server(app)
-const io = require("socket.io")(server, { cors: { origin: "*" } })
+
+const frontend_url = process.env.FRONTEND_URL || "https://skillup-community.netlify.app"
+
+// const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync("cert.pem"),
+//     passphrase: "myforumsshkey"
+// };
+
+const server = https.createServer(app) // initialize server here
+
+const io = require("socket.io")(server, { cors: { origin: "*" } }) // io can now access server
+
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-
-const frontend_url = process.env.FRONTEND_URL || "https://skillup-community.netlify.app"
 
 const PORT = process.env.PORT || 4040
 server.listen(PORT, () => console.log("Listening on port %d", PORT))
@@ -56,30 +64,7 @@ app.use((req, res, next) => {
     }
   });
 
-// app.use(function(req,res,next){
-//     try{
-//         if (req.url.substr(0, 18).indexOf("socket.io")>-1){
-//             //console.log("SOCKET.IO", req.url)
-//             return proxy.web(req, res, { target: 'wss://localhost:4567', ws: true }, function(e) { 
-//             //console.log('PROXY ERR',e)
-//             });
-//         } else {
-//             var target = proxyRules.match(req);
-//             if (target) {
-//                 //console.log("TARGET", target, req.url)
-//                 return proxy.web(req, res, {
-//                     target: target
-//                 }, function(e) { 
-//                 //console.log('PROXY ERR',e)
-//                 });
-//             } else {
-//                 res.sendStatus(404);
-//             }
-//         }
-//     } catch(e){
-//         res.sendStatus(500);
-//     }
-// });
+
 
 app.get("/", (req, res) => {
   res.status(200).json({success: true, message: "Wow everything is working fine!"})
@@ -111,32 +96,6 @@ app.use((err, req, res, _next) => {
 app.use((err, res, next) => {
   return res.status(400).json({success:false, message: "route not found"})
 })
-
-
-
-
-
-
-
-
-// const options = {/*Put your TLS options here.*/};
-
-
-
-
-
-// mainserver.listen(PORT, () => console.log("Listening on port %d", PORT))
-
-// mainserver.on('error', function (error, req, res) {
-//     let json;
-//     console.log('proxy error', error);
-//     if (!res.headersSent) {
-//     res.writeHead(500, { 'content-type': 'application/json' });
-//     }
-
-//     json = { error: 'proxy_error', reason: error.message };
-//     res.end(JSON.stringify(json));
-// });
 
 
 
